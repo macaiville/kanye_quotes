9b7aaaadaf922dfde55cfe50b765126782f4c46e
Jason Jones <43795154+macaiville@users.noreply.github.com>
2024-04-06 11:26:51 -0700

    Kanye Quotes

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ quote_text = canvas.create_text(150, 207, text=quote, width=250, font=("Arial", 
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=quote)
 kanye_button.grid(row=1, column=0)
 
 
